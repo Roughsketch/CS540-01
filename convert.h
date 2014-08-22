21b60b69f6d4bf7874d417268bf82a40c421e7fd
Maiddog <maiddogsrl@gmail.com>
2014-08-22 09:29:24 -0500

    Added basic header

---
+++ convert.h
@@ -0,0 +1,9 @@
+#ifndef CS540_CONVERT_H
+#define CS540_CONVERT_H
+
+//  String to Int
+int stoi(char *str, unsigned int base);
+
+void change_base(int n, int base, char *out, int max_len);
+
+#endif
